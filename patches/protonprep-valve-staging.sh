35941108d79f803894e4f4612a022e38a5dae5e4
MrLGamer <mrlgam3r@gmail.com>
2026-09-16 20:03:23 +0200

    Add HoYoverse game and launcher fixes

--- patches/protonprep-valve-staging.sh
+++ patches/protonprep-valve-staging.sh
@@ -475,6 +475,8 @@ apply_all_in_dir() {
     echo "WINE: -PROTON-ANIME- HACK: kernelbase dxgi redirect for ReShade"
     apply_patch "../patches/proton/0003-HACK-kernelbase-redirect-dxgi-for-ReShade.patch"
 
+    # HSR hacks ported from https://repo.tukandev.com/TTL-extras/sparkle
+    echo "WINE: -PROTON-ANIME- HACK: HSR wintrust catalog stub + first-request block (StarRail.exe)"
     echo "WINE: -PROTON-ANIME- HACK: NTE hide-launcher mode (PROTON_NTE_HIDE_LAUNCHER=1)"
     apply_all_in_dir "../patches/wine-hotfixes/proton-anime/"
 
